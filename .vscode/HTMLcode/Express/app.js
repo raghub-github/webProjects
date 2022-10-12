73c745dabcb53af94ccfe5bc4a19b638cc35ed6d
Raghu Bhunia <raghubhunia53@gmail.com>
2022-10-12 11:59:09 +0530

    html

--- .vscode/HTMLcode/Express/app.js
+++ .vscode/HTMLcode/Express/app.js
@@ -1,6 +1,22 @@
 const expess = require("express");
+const path = require("path");
 const app = expess();
 const port = 3001;
+
+// for serving static files
+app.use('/static', expess.static('static'));
+
+// set the template engine as pug
+app.set('view engine', 'pug');
+
+// set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// our pug demo endpoint 
+app.get("/demo",(req, res)=>{
+    res.status(200).render('demo', {title: 'Hey', massage: 'Hello There!'});
+})
+
 app.get("/",(req, res)=>{
     res.status(200).send("This is Homepage of my first express app");
 })
---
+++ .vscode/HTMLcode/Express/static/index.js
@@ -0,0 +1,1 @@
+console.log("I am not a good boy");
