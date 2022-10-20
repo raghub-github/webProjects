9863e9dcdae6dc940416950670dbcb4877dd74ce
Raghu Bhunia <raghubhunia53@gmail.com>
2022-10-20 12:26:52 +0530

    html

--- .vscode/HTMLcode/mongodb2.js
+++ .vscode/HTMLcode/mongodb2.js
@@ -14,4 +14,11 @@ db.items.find({rating: {$gte: 4.3}, price:{$gt: 4000}})
 db.items.find({$or: [{rating: {$gte: 4.3}}, {price:{$gt: 4000}}]})
 // geting perticular element of a object
 db.items.find({rating: {$gt: 4.3}}, {rating: 1})
-
+// Deleting item in smongo db
+// deleteOne will delete the matching entry and will delete the first entry in case of multi document match
+db.items.deleteOne({price: 22000})
+// Deleting similar items in smongo db in one line code
+db.items.deleteMany({price: 22000})
+// Updating the value in mongo db
+db.items.updateOne({name: "oppo 30f"},{$set: {price: 2}}) 
+db.items.updateMany({name: "oppo 30f"},{$set: {price: 2, rating: 1}}) 
