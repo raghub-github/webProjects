6071a8df80021d8156b28a85c203c897b586bcd6
Raghu Bhunia <raghubhunia53@gmail.com>
2022-10-20 15:41:50 +0530

    html

---
+++ .vscode/HTMLcode/mongoosInstall/index.js
@@ -0,0 +1,10 @@
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/raghukart', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    // we're connected!
+    console.log("We are connected bro...");
+});
