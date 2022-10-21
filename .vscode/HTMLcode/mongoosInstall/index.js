3de7ee7a0ca682e25960b48364ec36aad5e21b5b
Raghu Bhunia <raghubhunia53@gmail.com>
2022-10-21 13:39:29 +0530

    html

--- .vscode/HTMLcode/mongoosInstall/index.js
+++ .vscode/HTMLcode/mongoosInstall/index.js
@@ -1,10 +1,41 @@
 // getting-started.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/raghukart', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/raghukart', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
+db.once('open', function () {
     // we're connected!
     console.log("We are connected bro...");
 });
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+kittySchema.methods.speek = function(){
+    var greeting = "My name is " +this.name
+    console.log(greeting);
+}
+
+const kitten = mongoose.model('kitten', kittySchema);
+const raghuKittty = new kitten({name: 'raghuKitty'});
+const raghuKittty2 = new kitten({name: 'raghuKitty2'});
+console.log(raghuKittty.name);
+raghuKittty.speek();
+
+raghuKittty.save(function (err, raghuKittty){
+    if (err) return console.log(err);
+    raghuKittty.speek();
+})
+
+raghuKittty2.save(function (err, raghuKittty){
+    if (err) return console.log(err);
+    raghuKittty.speek();
+})
+
+kitten.find({name: "raghuKitty2"}, function(err, kittens){
+    if(err) return console.log(err);
+    console.log(kittens);
+});
+
