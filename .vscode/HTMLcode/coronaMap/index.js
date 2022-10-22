59f1a4685c0f49250009c2441905c3f5da978691
Raghu Bhunia <raghubhunia53@gmail.com>
2022-10-22 16:54:38 +0530

    html

--- .vscode/HTMLcode/coronaMap/index.js
+++ .vscode/HTMLcode/coronaMap/index.js
@@ -1,15 +1,15 @@
-function updateMap() 
-{
+function updateMap() {
     fetch("/data.json")
-        .then(resp => resp.json())
-        .then(rsp => {
-            console.log(rsp.data)
-            rsp.data.forEach(element => {
-                latitude = element.latitude;
-                longitude = element.longitude;
-                // Mark on the map
+    .then(resp => resp.json())
+    .then(rsp => {
+        console.log(rsp.data)
+        rsp.data.forEach(element => {
+            latitude = element.latitude;
+            longitude = element.longitude;
+            // Mark on the map
 
-            });
-        })
+        });
+    })
 }
+
 updateMap();
