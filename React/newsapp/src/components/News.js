fb6b3c593f438949ccad4ac55093387faae3dc46
Raghu <raghubhunia53@gmail.com>
2023-06-14 18:27:20 +0530

    done

---
+++ React/newsapp/src/components/News.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  constructor() {
+    super();
+    this.state = { articles: [] };
+  }
+  async componentDidMount() {
+    try {
+      let url =
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=5cb3f0dcd46f48d094c8e665b620a058";
+      const res = await fetch(url);
+      const data = await res.json();
+      this.setState({
+        articles: data.articles,
+      });
+    } catch (e) {
+      console.log("something is not working");
+    }
+  }
+  //   async componentDidMount() {
+  //     let url =
+  //       "https://newsapi.org/v2/top-headlines?country=in&apiKey=5cb3f0dcd46f48d094c8e665b620a058";
+  //     let data = await fetch(url);
+  //     let parseData = await data.json();
+  //     console.log(parseData);
+  //     this.setState({ articles: parseData.articles });
+  //   }
+  render() {
+    return (
+      <div className="container my-3 App">
+        <h2>NewsPlate - Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-sm-4" key={element.url}>
+                <NewsItem
+                  //   style={{maxWidth: '100%',
+                  //   display: '-webkit-box',
+                  //   WebkitBoxOrient: 'vertical',
+                  //   WebkitLineClamp: 2,
+                  //   overflow: 'hidden',
+                  //   textOverflow: 'ellipsis'}}
+                  //   title={element.title}
+                  //   description={element.description}
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
---
+++ React/newsapp/src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl} = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={!imageUrl?"https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/06_jun/13_tue/img_1686651417731_288.jpg?":imageUrl} className="card-img-top" alt="ImageUrl" />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
