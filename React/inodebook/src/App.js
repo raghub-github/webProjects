9d0bcb31990f74f92ac54b7a43de4dad4a19ba0a
Raghu <raghubhunia53@gmail.com>
2023-06-25 19:19:26 +0530

    done

--- React/inodebook/src/App.js
+++ React/inodebook/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import { Home } from "./component/Home";
+import About from "./component/About";
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
