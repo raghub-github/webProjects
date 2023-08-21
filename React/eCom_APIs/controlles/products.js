8d07c68730d442721c784b215782464c6e9a6e72
raghubhunia <raghubhunia53@gmail.com>
2023-08-21 21:37:47 +0530

    allset

--- React/eCom_APIs/controlles/products.js
+++ React/eCom_APIs/controlles/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
-    const { company, name, featured, sort } = req.query;
+    const { company, name, featured, sort, select } = req.query;
     const queryObject = {};
     if (company) {
         queryObject.company = { $regex: company, $options: "i" };
@@ -14,12 +14,23 @@ const getAllProducts = async (req, res) => {
     }
     let apiData = Product.find(queryObject);
     if (sort) {
-        let sortFix = sort.replace(",", " ")
+        let sortFix = sort.split(",").join(" ");
         apiData = apiData.sort(sortFix);
     }
-    const MyData = await apiData;
-    res.status(200).json({ MyData });
+    if (select) {
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
+    }
+
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 5;
+    let skip = (page - 1) * limit;
+    apiData = apiData.skip(skip).limit(limit);
+
+    const products = await apiData;
+    res.status(200).json({ products, nbHits:products.length });
 };
+
 const getAllProductsTesting = async (req, res) => {
     res.status(200).json(req.query).sort("name -price");
 };
