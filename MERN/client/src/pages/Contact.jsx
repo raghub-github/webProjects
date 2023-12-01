73519f9cb27c109ecd434aa28db406d79910637a
raghubhunia <raghubhunia53@gmail.com>
2023-12-01 19:40:50 +0530

    done

--- MERN/client/src/pages/Contact.jsx
+++ MERN/client/src/pages/Contact.jsx
@@ -1,4 +1,93 @@
+import { useState } from "react";
+
 export const Contact = () => {
-    return <h1>Hello contact</h1>;
+  const [contact, setContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setContact({ ...contact, [name]: value });
   };
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contact);
+  };
+
+  return (
+    <>
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">Contact Us</h1>
+        </div>
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img
+              src="/images/support.png"
+              alt="we are always ready to help you"
+            />
+          </div>
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  required
+                  onChange={handleInput}
+                  value={contact.username}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  required
+                  onChange={handleInput}
+                  value={contact.email}
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  autoComplete="off"
+                  required
+                  id="message"
+                  cols="30"
+                  rows="3"
+                  onChange={handleInput}
+                  value={contact.message}
+                ></textarea>
+              </div>
+              <div>
+                <button type="submit">submit</button>
+              </div>
+            </form>
+          </section>
+        </div>
+        <section className="mb-3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d114748.2169537054!2d87.47558792516789!3d21.947620190674698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDU1JzUwLjAiTiA4N8KwMjcnMzYuNyJF!5e0!3m2!1sen!2sin!4v1701435818595!5m2!1sen!2sin"
+            width="97%"
+            height="450"
+            allowFullscreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            style={{ margin: "auto", display: "block"}}
+          ></iframe>
+        </section>
+      </section>
+    </>
+  );
+};
--- MERN/client/src/pages/Home.jsx
+++ MERN/client/src/pages/Home.jsx
@@ -1,7 +1,89 @@
 export const Home = () => {
   return (
     <>
-      <h1>Hello Dost</h1>
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>welcome to React app</p>
+              <h1>welcome again to React app</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellendus, delectus, voluptates, illum reiciendis sunt
+                excepturi consequatur optio fugit cupiditate et quisquam odio
+                mollitia!
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">connect now</button>
+                </a>
+                <a href="/services">
+                  <button className="btn secondary-btn">learn more</button>
+                </a>
+              </div>
+            </div>
+            <div className="hero-image">
+              <img
+                src="images/home.png"
+                alt="coding together"
+                width="450"
+                height="500"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <section className="section-analytics">
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>Registered Companies</p>
+          </div>
+          <div className="div1">
+            <h2>100,00+</h2>
+            <p>Happy Clients</p>
+          </div>
+          <div className="div1">
+            <h2>500+</h2>
+            <p>Well known Devlopers</p>
+          </div>
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>Service</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          <div className="hero-image">
+            <img
+              src="images/design.png"
+              alt="coding together"
+              width="450"
+              height="500"
+            />
+          </div>
+          <div className="hero-content">
+            <p>we are here to help you</p>
+            <h1>we are here to help you</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus, delectus, voluptates, illum reiciendis sunt excepturi
+              consequatur optio fugit cupiditate et quisquam odio mollitia!
+            </p>
+            <div className="btn btn-group">
+              <a href="/services">
+                <button className="btn secondary-btn">learn more</button>
+              </a>
+              <a href="/contact">
+                <button className="btn">connect now</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
