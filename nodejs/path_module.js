e35436d2a99873d664c488ed3e30ccbcaffe3033
raghubhunia <raghubhunia53@gmail.com>
2024-12-21 11:34:34 +0530

    commit

---
+++ nodejs/path_module.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(path)
+
+const filePath = path.join("folder", "students","data.txt");
+console.log(filePath)
