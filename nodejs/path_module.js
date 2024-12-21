e879e8e55671b37298b90d51216574b88f29197a
raghubhunia <raghubhunia53@gmail.com>
2024-12-21 12:11:59 +0530

    path

--- nodejs/path_module.js
+++ nodejs/path_module.js
@@ -6,3 +6,11 @@ const path = require("path");
 
 const filePath = path.join("folder", "students","data.txt");
 console.log(filePath)
+
+const parseData = path.parse(filePath);
+const resolvedPath = path.resolve(filePath);
+const extname = path.extname(filePath);
+const basename = path.basename(filePath);
+const dirname = path.dirname(filePath);
+
+console.log({parseData, resolvedPath, extname, basename, dirname});
